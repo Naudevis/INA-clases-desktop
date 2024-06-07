c13d19eac10af9c3c282dc88df4b9093e2bfb1b7
Keneth-F <kenethfg123@gmail.com>
2024-06-07 15:00:17 -0600

    finishing products

--- App1/Forms/Main.cs
+++ App1/Forms/Main.cs
@@ -16,16 +16,40 @@ namespace App1.Forms
         {
             InitializeComponent();
         }
-        string[] names = new string[3];
-        int[] units = new int[3];
-        double[] prices = new double[3];
-        double[] totals = new double[3];
+        string[] names = new string[1];
+        int[] units = new int[1];
+        double[] prices = new double[1];
+        double[] totals = new double[1];
         private void materialButton1_Click(object sender, EventArgs e)
         {
             var name= materialTextBox21.Text;
             int unit = materialSlider1.Value;
             double price = Double.Parse(materialTextBox22.Text);
             double total = Double.Parse(materialTextBox23.Text);
+            if (total<=0 || unit <= 0 || price <= 0)
+            {
+                MessageBox.Show("no");
+            }
+            else
+            {
+                if(string.IsNullOrEmpty(name))
+                {
+                    MessageBox.Show("no name");
+                }
+                else
+                {
+                    Array.Resize(ref names, names.Length+1);
+                    Array.Resize(ref units, units.Length + 1);
+                    Array.Resize(ref prices, prices.Length + 1);
+                    Array.Resize(ref totals, totals.Length + 1);
+                    names[names.Length - 1] = name;
+                    units[units.Length - 1]= unit;
+                    prices[ prices.Length - 1]= price;
+                    totals[ totals.Length - 1]= total;
+                    MessageBox.Show("si");
+                    dataGridView1.Rows.Add(new object[] {name, unit, price, total});
+                }
+            }
         }
     }
 }
